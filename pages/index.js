36f8c0e686d632b732736765dd9f4366e0243684
Stranger Mushwana <strangermushwana@gmail.com>
2023-03-21 16:06:57 +0200

    added events api route

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Layout from '@components/Layout'
+import { API_URL } from '@config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export async function getStaticSideProps() {
+  const events = await (await fetch(`${API_URL}/api/events`)).json()
+  return {
+    props: { events },
+    revalidate: 1
+  }
 }
