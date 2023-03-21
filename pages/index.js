ab75cb69abff3b92598317ea2fba002138fc093d
Stranger Mushwana <strangermushwana@gmail.com>
2023-03-21 16:33:46 +0200

    added event ui and event item

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
+import Link from 'next/link'
 import Layout from '@components/Layout'
+import EventItem from '@components/EventItem'
 import { API_URL } from '@config/index'
 
 export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      { events.length === 0 && <h3>No events to show</h3> }
+      { events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      )) }
+      { events.length > 0 && (
+        <Link legacyBehavior href='/events' className="btn-secondary">
+          <a>View All Events</a>
+        </Link>
+      ) }
     </Layout>
   )
 }
 
-export async function getStaticSideProps() {
-  const events = await (await fetch(`${API_URL}/api/events`)).json()
+export async function getStaticProps() {
+  const res = await await fetch(`${API_URL}/api/events`)
+  let events = await res.json()
   return {
-    props: { events },
+    props: { events: events.slice(0,3) },
     revalidate: 1
   }
 }
