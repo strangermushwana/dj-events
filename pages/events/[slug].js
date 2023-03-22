b98d20ad8ed7bbfddc3d44f022e918274c30ba03
Stranger Mushwana <strangermushwana@gmail.com>
2023-03-22 19:14:12 +0200

    added route for a single event

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,40 @@
-import Layout from '@components/Layout'
+import Layout from "@components/Layout"
+import { API_URL } from "@config/index"
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <div>
+
+			</div>
     </Layout>
   )
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const event = await res.json()
+//   return {
+//     props: { event: event[0] }
+//   }
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map((evt) => ({ params: { slug: evt.slug } }))
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+  return {
+    props: { event: events[0] },
+    revalidate: 1,
+  }
 }
